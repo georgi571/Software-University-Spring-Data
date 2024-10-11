2c68c62075806a0211035c996e5dabe76c3ddc79
Georgi Kostadinov <georgi93571@gmail.com>
2024-10-12 00:27:48 +0300

    add solution

---
+++ src/main/java/_8SpringData/_1SpringData/_8JSONProcessing/_2Exercise/_2CarDealer/org/softwareuniversityspringdata/util/adapters/LocalDateTimeAdapter.java
@@ -0,0 +1,25 @@
+package _8SpringData._1SpringData._8JSONProcessing._2Exercise._2CarDealer.org.softwareuniversityspringdata.util.adapters;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+
+
+    @Override
+    public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
+        jsonWriter.value(localDateTime.format(formatter));
+    }
+
+    @Override
+    public LocalDateTime read(JsonReader jsonReader) throws IOException {
+        return LocalDateTime.parse(jsonReader.nextString(), formatter);
+    }
+}
